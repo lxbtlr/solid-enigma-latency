81fe20eb97116881b36cc7ee2d11ecc6da1cdf81
Alex Butler <lxbtlr@pm.me>
2025-02-13 12:03:21 -0600

    Add volatile on shared memory to fix race condition

--- pingpong.cpp
+++ pingpong.cpp
@@ -1,11 +1,13 @@
+#include <atomic>
 #include <bitset>
 #include <iostream>
+#include <limits>
+#include <math.h>
 #include <pthread.h> // pthread api
 #include <sched.h>   // for processor affinity
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-
 /*
  * thread to thread latency test.
  * Times collected should be the combination of trip to thread, write to data,
@@ -15,10 +17,24 @@
  */
 
 pthread_t* tid;
-int shared_mem = 0;
+long* data;
+// need volatile to prevent compiler interfering
+volatile int shared_mem = 0;
+
+struct stats {
+  // min, max, aavg, gavg
+  uint64_t min;
+  uint64_t max;
+  uint64_t aavg;
+  uint64_t gavg;
+};
+
+#define NUM_ITERS 100
+#define INT_MAX 0x7fffffff
+#define INT_MIN 0
 
 #define uchar_t uint8_t
-// Timer mechanism using rdtsc (?)
+// Tim64_ter mechanism using rdtsc (?)
 #define rdtscll(val)                          \
   do {                                        \
     uint64_t tsc;                             \
@@ -27,6 +43,18 @@ int shared_mem = 0;
     *(uint32_t*)&(tsc) = a;                   \
     *(uint32_t*)(((uchar_t*)&tsc) + 4) = d;   \
     val = tsc;                                \
+  } while (0)
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+#define SUM(val, _data)                                          \
+  do {                                                           \
+    uint64_t sum = 0;                                            \
+    for (int j = 0; j < (sizeof(_data) / sizeof(*_data)); j++) { \
+      sum += _data[j];                                           \
+    }                                                            \
+    val = sum;                                                   \
   } while (0)
 
 void* ping(void* input)
@@ -115,6 +143,7 @@ long set_pingpong(long thread1, long thread2) //, int num_procs)
 
 int main(int argc, char* argv[])
 {
+  int i;
   long t1, t2;
   long pingpong_time;
 
@@ -127,6 +156,27 @@ int main(int argc, char* argv[])
 
   tid = (pthread_t*)malloc(sizeof(pthread_t) * 2 /*num threads */);
 
-  printf("Start PP\n");
-  printf("latency:\t %lu clk cycles\n", (long)set_pingpong(t1, t2));
+  printf("Start PingPong\n");
+
+  // malloc the data array
+  data = (long*)malloc(sizeof(long) * NUM_ITERS);
+
+  uint64_t lmin = INT_MAX;
+  uint64_t lmax = INT_MIN;
+  uint64_t aavg, gavg;
+
+  printf("Start Tests\n");
+  for (i = 0; i < NUM_ITERS; i++) {
+    long current = (long)set_pingpong(t1, t2);
+    lmin = MIN(lmin, current);
+    lmax = MAX(lmax, current);
+    data[i] = current;
+    printf("iter: %i complete\n", i);
+  }
+  printf("Calc stats\n");
+  SUM(aavg, data);
+  stats single = { .min = lmin, .max = lmax, .aavg = aavg, .gavg = gavg };
+
+  // printf("latency:\t %lu clk cycles\n", (long)set_pingpong(t1, t2));
+  printf("min: %lu\nmax: %lu\naavg: %lu\ngavg: %lu\n", single.min, single.max, single.aavg, single.gavg);
 }
