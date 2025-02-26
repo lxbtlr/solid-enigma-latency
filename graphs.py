d721827faff2dec7df914e6cdeaa421bbdaa7864
Alex Butler <lxbtlr@pm.me>
2025-02-26 14:20:27 -0600

    WIP: Add graphs.py, Update pingpong -- Now using shared memory pages explicitly defined.
    Thank you kevin for improving this so much, many thanks.
    Now incorporating macros for creating memory, add struct for shared data
    and pointing to the data. Add memrst function -> should be a macro.

---
+++ graphs.py
@@ -0,0 +1,106 @@
+import pandas 
+import numpy 
+import matplotlib.pyplot as mpl
+import argparse
+import seaborn
+from scipy.cluster import hierarchy 
+import networkx
+
+from enum import Enum
+
+class Graph(Enum):
+    heatmap=1
+    kmeans=2
+    dendrite=3
+
+
+parser = argparse.ArgumentParser(prog='Latency Graphs',
+                                 description="Visualize latency data",
+                                 epilog="choose a graph type, and send data in")
+
+# TODO: add more args as needed
+parser.add_argument("filename",help="data file")
+parser.add_argument("-g","--graph",help="graph type [heatmap, kmeans, dendrites]")
+parser.add_argument("-t","--title",help="graph title")
+#parser.add_argument("")
+
+
+
+# TODO: add missing args errors
+
+
+
+def read_file(fname:str):
+    """
+    Read csv, and output local and globals df's
+    """
+    _df = pandas.read_csv(fname)
+    _globals = _df[_df.stat == "g"]
+    _locals = _df[_df.stat == "l"]
+    
+    return _globals, _locals
+
+def clean(input_data):
+
+    output_data = input_data
+
+    return output_data
+
+
+def dendrite():
+    """
+    Make a dendrite diagram from peter's description    
+    """
+
+    # TODO: find a lib that will do this for us or implement
+
+    pass
+
+def kmeans(k,input_data):
+    """
+    generate k-means from data points, also return notion of how well they fit
+    
+    """
+    # TODO: find a lib that will do this for us or implement
+    
+    return k
+
+
+def heatmap(input_data):
+    """
+    generate a heatmap to visualize latency data
+        
+      t1 t2 t3 t4 
+   t1 xX xx xx xX 
+   t2 xx xx xx XX
+   t3 XX Xx xx xX
+   t4 xx xx xx XX
+
+    """
+    print(input_data)
+    _data = input_data.pivot(index="t1",
+                             columns="t2",
+                             values="min").fillna(0)
+    print(_data)
+    f, ax = mpl.subplots(figsize=(9, 6))
+    print("hello")
+    seaborn.heatmap(_data, ax=ax,)
+    mpl.show()
+    pass
+
+if __name__ == "__main__":
+    
+    args = parser.parse_args()
+    graph = args.graph
+
+    globals_df, locals_df = read_file(args.filename)
+
+    if int(graph) == Graph.heatmap.value:
+        heatmap(locals_df)
+    elif graph == Graph.kmeans.value:
+        pass
+    elif graph == Graph.dendrite.value:
+        pass
+
+    pass
+
--- pingpong.cpp
+++ pingpong.cpp
@@ -1,9 +1,11 @@
+#include <cstdint>
 #include <math.h>
 #include <pthread.h> // pthread api
 #include <sched.h>   // for processor affinity
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <unistd.h>
 /*
  * thread to thread latency test.
@@ -43,12 +45,46 @@
 
 FILE* f = fopen("test.out", "w");
 
+struct sharedm { // thank you kevin for the more cogent solution to this
+  uint64_t tid;
+  uint64_t cpu;
+  uint64_t* read_addr; // TODO: double check if we need volatile flags here
+  uint64_t* write_addr;
+};
+
+#define mk_shared_data(thread1, thread2) \
+  void* serve = mmap(NULL, 0x1000,       \
+      PROT_READ | PROT_WRITE,            \
+      MAP_SHARED | MAP_ANONYMOUS,        \
+      0, 0);                             \
+                                         \
+  memset(serve, 0, 0x1000);              \
+                                         \
+  void* volley = mmap(NULL, 0x1000,      \
+      PROT_READ | PROT_WRITE,            \
+      MAP_SHARED | MAP_ANONYMOUS,        \
+      0, 0);                             \
+  memset(volley, 0, 0x1000);             \
+  struct sharedm t1 {                    \
+    .tid = tid[thread1],                 \
+    .cpu = thread1,                      \
+    .read_addr = (uint64_t*)volley,      \
+    .write_addr = (uint64_t*)serve,      \
+  };                                     \
+  struct sharedm t2 {                    \
+    .tid = tid[thread2],                 \
+    .cpu = thread2,                      \
+    .read_addr = (uint64_t*)serve,       \
+    .write_addr = (uint64_t*)volley,     \
+  };
+
 struct stats {
   // min, max, aavg, gavg
   uint64_t min;
   uint64_t max;
   uint64_t aavg;
 };
+
 int NUM_THREADS;
 int AVOID_HT;
 
@@ -59,63 +95,74 @@ long* pair_data;
 // need volatile to prevent compiler interfering
 volatile int shared_mem = 0;
 
+void memrst(sharedm data)
+{
+  memset(data.write_addr, 0, 0x1000);
+  memset(data.read_addr, 0, 0x1000);
+}
+
 void* ping(void* input)
 {
 
-  long t_id = (long)input;
+  struct sharedm* d = (sharedm*)input;
+  // long t_id = (long)input;
   cpu_set_t set;
   CPU_ZERO(&set);
-  CPU_SET(t_id, &set);
+  // printf("ping:\t%lu\n", d->cpu);
+  CPU_SET(d->cpu, &set);
   if (sched_setaffinity(0, sizeof(set), &set) < 0) {
     perror("Can't setaffinity");
     exit(-1);
   }
-
-  // Sender Thread
 
   uint64_t start, end;
   // should start the clock and write to data in memory
   rdtscll(start);
 
-  shared_mem = 1; // write to memory
-  while (shared_mem == 1) {
-
+  *d->write_addr = 1;          // serve ball
+  while (*d->read_addr == 0) { // wait for return
     // sit and wait
   }
   rdtscll(end);
+  //  NOTE: RESET VAL
+
   void* time = (void*)(end - start);
+
+  //  Sender Thread
+
 #if VERBOSE
-  fprintf(stderr, "[INFO] Exiting thread %lu\n", t_id);
+  fprintf(stderr, "[INFO] Exiting thread %lu\n", d->cpu);
 #endif
-  pthread_exit(time);
+  pthread_exit((void*)time);
 }
 
 void* pong(void* input)
 {
   // Receiver Thread
-  long t_id = (long)input;
+  struct sharedm* d = (sharedm*)input;
+  // old system:
   cpu_set_t set;
   CPU_ZERO(&set);
-  CPU_SET(t_id, &set);
+  CPU_SET(d->cpu, &set);
   if (sched_setaffinity(0, sizeof(set), &set) < 0) {
     perror("Can't setaffinity");
     exit(-1);
   }
+
   // should observe the change in memory,
-  while (shared_mem != 1) {
+  while (*d->read_addr == 0) {
     // sit and wait
   }
-  shared_mem = 2;
-  // then overwrite (restore) the value
+  *d->write_addr = 1;
 
 #if VERBOSE
-  fprintf(stderr, "[INFO] Exiting thread %lu\n", t_id);
+  fprintf(stderr, "[INFO] Exiting thread %lu\n", d->cpu);
 #endif
   // then exit
   pthread_exit(NULL);
 }
 
-long set_pingpong(long thread1, long thread2) //, int num_procs)
+long set_pingpong(sharedm thread1, sharedm thread2) //, int num_procs)
 {
   void* time;
   // Set up the threads
@@ -123,37 +170,38 @@ long set_pingpong(long thread1, long thread2) //, int num_procs)
 #if VERBOSE
   printf("Start threads\n");
 #endif
-  start_condition = pthread_create(&(tid[thread1]),
+  start_condition = pthread_create(&(thread1.tid),
       NULL,
       pong,
-      (void*)thread1);
+      (void*)&thread1);
 
-  if (start_condition == 0) {
 #if VERBOSE
+  if (start_condition == 0) {
     printf("[INFO] PONG started correct\n");
-#endif
   }
+#endif
 
-  start_condition = pthread_create(&(tid[thread2]),
+  start_condition = pthread_create(&(thread2.tid),
       NULL,
       ping,
-      (void*)thread2);
+      (void*)&thread2);
 
-  if (start_condition == 0) {
 #if VERBOSE
+  if (start_condition == 0) {
     printf("[INFO] PING started correct\n");
-#endif
   }
+#endif
   // rejoin threads
-  pthread_join(tid[thread1], NULL);
-  pthread_join(tid[thread2], &time);
+  pthread_join(thread2.tid, &time);
+#if VERBOSE
+  printf("[INFO] PING joined correct\n");
+#endif
 
-  shared_mem = 0; // reset mem
-
+  memrst(thread1);
   return (long)time;
 }
 
-stats pair(const long thread1, const long thread2)
+stats pair(const uint64_t thread1, const uint64_t thread2)
 {
 
   int i;
@@ -161,16 +209,21 @@ stats pair(const long thread1, const long thread2)
   uint64_t lmax = INT_MIN;
   uint64_t aavg;
 
+  tid = (pthread_t*)malloc(sizeof(pthread_t) * (2) /*num threads */);
+
+  mk_shared_data(thread1, thread2);
+
   pair_data = (long*)malloc(sizeof(long) * NUM_ITERS);
 
   for (i = 0; i < NUM_ITERS; i++) {
-    long current = (long)set_pingpong(thread1, thread2);
+    long current = (long)set_pingpong(t1, t2);
     lmin = MIN(lmin, current);
     lmax = MAX(lmax, current);
     pair_data[i] = current;
 #if VERBOSE
-    printf("iter: %i complete\n", i);
+    printf("iter: %li complete\n", i);
 #endif
+    // memrst(t1);
   }
 #if VERBOSE
   printf("Calc stats\n");
@@ -188,7 +241,7 @@ stats pair(const long thread1, const long thread2)
   return single;
 }
 
-stats tournament(const long prima, const int beg, const int last)
+stats tournament(const uint64_t prima, const int beg, const int last)
 {
   // Ping Pong Thread Tournament,
   // Generate the min and avg scores from ping pong test of one thread to all
@@ -205,19 +258,21 @@ stats tournament(const long prima, const int beg, const int last)
   uint64_t gmax = INT_MIN;
   uint64_t gaavg = 0;
 
+  tid = (pthread_t*)malloc(sizeof(pthread_t) * (last - beg) /*num threads */);
   // NOTE: sweep from first thread to last thread, excluding primary thread
-  for (int i = beg; i <= last; i++) {
+  for (uint64_t i = beg; i <= last; i++) {
     if (i == prima) {
       // TODO:[x] catch case where we are playing ping pong with our self
     } else {
       pdata = (long*)malloc(sizeof(long) * NUM_ITERS);
 
+      mk_shared_data(prima, i);
       uint64_t lmin = INT_MAX;
       uint64_t lmax = INT_MIN;
       uint64_t aavg;
 
       for (int j = 0; j < NUM_ITERS; j++) {
-        long current = (long)set_pingpong(prima, i);
+        long current = (long)set_pingpong(t1, t2);
         lmin = MIN(lmin, current);
         lmax = MAX(lmax, current);
         pdata[j] = current;
@@ -227,7 +282,7 @@ stats tournament(const long prima, const int beg, const int last)
       }
       SUM(aavg, pdata);
 
-      fprintf(f, "l,%lu,%i,%lu,%lu,%lu,\n", prima, i, lmin, lmax, aavg);
+      fprintf(f, "l,%lu,%li,%lu,%lu,%lu,\n", prima, i, lmin, lmax, aavg);
 
       gmin = MIN(gmin, lmin);
       gmax = MAX(gmax, lmax);
@@ -251,18 +306,17 @@ int main(int argc, char* argv[])
 {
   int mode;
   long t1, t2;
-  // fflush(stdout);
-  if (argc != 5) {
+
+  if (argc != 4) {
     // TODO: bake modes into this (thread pairs vs thread sweeps)
-    printf("usage: pingpong thread_1 thread_2 Avoid_HT Mode\n");
+    printf("usage: pingpong thread_1 thread_2 Mode\n");
     exit(-1);
   }
-  t1 = atoi(argv[1]);       // thread 1
-  t2 = atoi(argv[2]);       // thread 2
-  AVOID_HT = atoi(argv[3]); // avoid hyperthreading?
-  mode = atoi(argv[4]);     // op mode
+  t1 = atoi(argv[1]); // thread 1
+  t2 = atoi(argv[2]); // thread 2
+  // AVOID_HT = atoi(argv[3]); // avoid hyperthreading?
+  mode = atoi(argv[3]); // op mode
 
-  tid = (pthread_t*)malloc(sizeof(pthread_t) * (t2 - t1) /*num threads */);
   // pthread_t tid[t2];
   //  malloc the data array
 #if VERBOSE
@@ -274,6 +328,7 @@ int main(int argc, char* argv[])
     printf("pair mode\n");
 #endif
     printf("tid,min,max,aavg,NUM_ITERS,\n");
+
     stats pair_mode = pair(t1, t2);
     break;
   }
@@ -281,10 +336,6 @@ int main(int argc, char* argv[])
 #if VERBOSE
     printf("Tournament mode\n");
 #endif
-    // NOTE: THIS ONE
-    // fprintf(f, "stat,t1,t2,min,max,aavg,\n");
-
-    // FIXME:
     fprintf(f, "stat,t1,t2,min,max,aavg,\n");
     heatmap = (stats*)malloc(sizeof(stats) * (t2 - t1) /*num threads */);
     for (int thread_num = t1; thread_num <= t2; thread_num++) {
@@ -297,5 +348,5 @@ int main(int argc, char* argv[])
     fprintf(stderr, "\t\t\t\t\t^^Mode not Mapped\n");
   }
   }
-  // fclose(f);
+  // mlockall(MCL_CURRENT | MCL_FUTURE);
 }
