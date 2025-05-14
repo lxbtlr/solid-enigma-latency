b2d20f928fd4903c2482fd14d44011b74c7ac58b
Alex Butler <lxbtlr@pm.me>
2025-05-14 14:50:25 -0500

    dd scaffolds for new test for the arm latency tester

--- dcache.c
+++ dcache.c
@@ -38,12 +38,12 @@ uint64_t ntrials = NUM_TRIALS;
 #define uchar_t uint8_t
 // Timer mechanism using rdtsc (?)
 
-uint64_t alaska_timestamp() {
+uint64_t alaska_timestamp()
+{
   struct timespec spec;
   clock_gettime(1, &spec);
   return spec.tv_sec * (1000 * 1000 * 1000) + spec.tv_nsec;
 }
-
 
 #ifdef __x86_64__
 
@@ -52,17 +52,16 @@ uint64_t alaska_timestamp() {
   do {                                        \
     uint64_t tsc;                             \
     uint32_t a, d;                            \
-	asm volatile("rdtsc" : "=a"(a), "=d"(d)); \
+    asm volatile("rdtsc" : "=a"(a), "=d"(d)); \
     *(uint32_t*)&(tsc) = a;                   \
     *(uint32_t*)(((uchar_t*)&tsc) + 4) = d;   \
     val = tsc;                                \
   } while (0)
-#else 
+#else
 
 #define AMORTIZED_RUNS 100
-#define rdtscll(val) val = alaska_timestamp()  
+#define rdtscll(val) val = alaska_timestamp()
 #endif
-
 
 #define MIN(x, y) (((uint64_t)x) < ((uint64_t)y) ? ((uint64_t)x) : ((uint64_t)y))
 #define MAX(x, y) (((uint64_t)x) > ((uint64_t)y) ? ((uint64_t)x) : ((uint64_t)y))
@@ -124,6 +123,94 @@ typedef struct {
 } __attribute__((packed)) __attribute__((aligned(PAGE_SIZE))) passes;
 
 // NOTE: FIX ALL THE UNNECESSARY IO
+
+void* arm_ping(void* _g)
+{
+  passes* g = _g;
+  long myid = (long)(g->player1);
+
+  uint64_t* rec_times = malloc(sizeof(uint64_t) * ntrials);
+
+  cpu_set_t set;
+  CPU_ZERO(&set);
+  // printf("ping:\t%lu\n", d->cpu);
+  CPU_SET(myid, &set);
+  if (sched_setaffinity(0, sizeof(set), &set) < 0) {
+    perror("Can't setaffinity");
+    exit(-1);
+  }
+
+  uint64_t start;
+  uint64_t stop;
+  // should start the clock and write to data in memory
+  uint64_t runs = 0;
+
+  uint64_t state;
+  for (uint64_t trial = 0; trial < NUM_TRIALS; trial++) {
+    *g->first = 1;
+    asm volatile("dmb ish;"
+                 "dsb ish;"
+                 "isb sy;" :::);
+    rdtscll(start);
+#ifdef AMORTIZED_RUNS
+    for (uint64_t y = 0; y < AMORTIZED_RUNS; y++) {
+#else
+    for (uint64_t y = 0; y < 100; y++) {
+#endif
+      // update state
+      state = y % 2;
+      // NOTE: this is the new control flow based gadget for dcache
+
+      *g->first = state;
+
+      while (*g->second == (state == 0 ? 1 : 0)) { // wait for return
+        // sit and wait
+      }
+    }
+    rdtscll(stop);
+    rec_times[trial] = (uint64_t)stop - start;
+    pthread_barrier_wait(&barrier);
+  }
+
+  pthread_exit(rec_times);
+}
+
+void* arm_pong(void* _g)
+{
+  passes* g = _g;
+  long myid = (long)(g->player2);
+  cpu_set_t set;
+  CPU_ZERO(&set);
+  CPU_SET(myid, &set);
+  if (sched_setaffinity(0, sizeof(set), &set) < 0) {
+    perror("Can't setaffinity");
+    exit(-1);
+  }
+
+  uint64_t state;
+  // NOTE: m1(serve), m2(volley) should be 0, 1; respectively
+  for (uint64_t trial = 0; trial < NUM_TRIALS; trial++) {
+    *g->second = 1;
+
+#ifdef AMORTIZED_RUNS
+    for (uint64_t x = 0; x < AMORTIZED_RUNS; x++) {
+#else
+    for (uint64_t x = 0; x < 100; x++) {
+#endif
+      // update state
+      state = x % 2; // 0 or 1
+      // should observe the change in memory,
+      while (*g->first == state) {
+        // sit and wait
+      }
+      *g->second = (state == 0 ? 1 : 0); // not state
+    }
+    pthread_barrier_wait(&barrier);
+  }
+
+  pthread_exit(NULL);
+}
+
 void* ping2(void* _g)
 {
   passes* g = _g;
@@ -152,24 +239,7 @@ void* ping2(void* _g)
     fprintf(stderr, DBG "ping: trial %lu\n", trial);
 #endif
 
-
-#ifdef AMORTIZED_RUNS
-    rdtscll(start);
-    for (runs = 0; runs<AMORTIZED_RUNS ; runs++){
-       *g->first = 1;
-
-       while (*g->second == 0) { // wait for return
-         // sit and wait
-       }
-       // NOTE: we no longer print each iter
-       //  printf("%lu,%lu,%lu,%i\n", g->player1, g->player2, trial, time);
-       *g->first = 0; // Reset
-    }
-    rdtscll(stop);
-    rec_times[trial] = (uint64_t)(stop - start)/AMORTIZED_RUNS;
-    
-    printf("%lu, b:%lu, e:%lu\n",rec_times[trial], start, stop);
-#else    
+    // NOTE: this is the new control flow based gadget for dcache
     rdtscll(start);
 
     *g->first = 1;
@@ -183,7 +253,6 @@ void* ping2(void* _g)
     *g->first = 0; // Reset
     rec_times[trial] = (uint64_t)stop - start;
     FORCE_SERIAL;
-#endif
 #if VERBOSE
     fprintf(stderr, DBG "ping: finished\n");
 #endif
@@ -208,10 +277,7 @@ void* pong2(void* _g)
 #endif
   for (uint64_t trial = 0; trial < ntrials; trial++) {
     // should observe the change in memory,
-    
-#ifdef AMORTIZED_RUNS
-    for (uint64_t j = 0; j < AMORTIZED_RUNS; j++) {
-#endif
+
     while (*g->first == 0) {
       // sit and wait
     }
@@ -224,14 +290,10 @@ void* pong2(void* _g)
     while (*g->first != 0) { }
     *g->second = 0; // Reset
 
-#ifdef AMORTIZED_RUNS
-    }
-#endif
-
 #if VERBOSE
     fprintf(stderr, DBG "pong: finished\n");
 #endif
-    //FORCE_SERIAL;
+    // FORCE_SERIAL;
     pthread_barrier_wait(&barrier);
   }
 
